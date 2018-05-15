a052daecfdba47331acc77e765cc398d564c3f4a
Doan Le Thieu <thieurom@gmail.com>
2018-05-15 10:30:19 +0700

    Add Quote model

---
+++ ProgrammingQuotes/Quote.swift
@@ -0,0 +1,14 @@
+//
+//  Quote.swift
+//  ProgrammingQuotes
+//
+//  Created by Doan Le Thieu on 5/15/18.
+//  Copyright © 2018 Doan Le Thieu. All rights reserved.
+//
+
+import Foundation
+
+struct Quote {
+    var text: String
+    var authorName: String
+}
---
+++ ProgrammingQuotesTests/QuoteTests.swift
@@ -0,0 +1,33 @@
+//
+//  QuoteTests.swift
+//  ProgrammingQuotesTests
+//
+//  Created by Doan Le Thieu on 5/15/18.
+//  Copyright © 2018 Doan Le Thieu. All rights reserved.
+//
+
+import XCTest
+@testable import ProgrammingQuotes
+
+class QuoteTests: XCTestCase {
+    
+    var quote: Quote!
+    
+    override func setUp() {
+        super.setUp()
+        
+        quote = Quote(text: "This is a quote", authorName: "John")
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+ 
+    func testQuoteHasText() {
+        XCTAssertEqual(quote.text, "This is a quote")
+    }
+    
+    func testQuoteHasAuthorName() {
+        XCTAssertEqual(quote.authorName, "John")
+    }
+}
